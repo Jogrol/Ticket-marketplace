84429d28885848312c83998e59b6b42de8ed89bb
Joey Grolleman <joeygrolleman@gmail.com>
2019-02-26 09:05:32 +0100

    filter events by date applied on server side. Consider refactor code and apply filter to client side

--- server/src/events/controller.ts
+++ server/src/events/controller.ts
@@ -1,5 +1,6 @@
 import {Controller, Get, HttpCode, Body, Post} from 'routing-controllers'
 import Event from './entity'
+import * as moment from 'moment'
 
 @Controller()
 export default class MainController {
@@ -7,7 +8,11 @@ export default class MainController {
     @Get('/events')
     async allEvents() {
         const events = await Event.find();
-        return { events };
+        const today = moment().format('L');
+        const filteredEvents = await events.filter(event =>  moment(event.end_date).format('L') > today)
+        console.log(filteredEvents)
+        return { filteredEvents };
+
     }
     @Post("/events")
     @HttpCode(201)
